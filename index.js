d70db5cb8722c44f516cd4fcc1838d4123c714dd
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2017-03-21 12:44:03 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var net = require('net');
+var delayed = require('delayed');
+
+var address = '128.199.66.43';
+var port = 47240;
+var connection = 3000;
+var count = 0;
+
+for(var i = 0; i < connection; i++)
+{
+    delayed.delay(() => { 
+        var client = new net.Socket();
+        client.connect(port, address, function() {
+            count++;
+            console.log('Connected for: ' + count);
+            client.on('error', function(err){
+                client.write('err: ' + err);
+            });
+        })
+    }, 100 * i);
+}
